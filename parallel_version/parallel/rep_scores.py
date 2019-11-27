fa81d6390787ddf2b0c0ef0396428fd20bb31c00
Roberto Bellarosa <37309982+RobertoBellarosa95@users.noreply.github.com>
2019-11-27 20:41:04 +0100

    Create rep_scores.py
    Un file che contiene tutti i valori per calcolare adeguatamente la reputazione, e anche per modificare quando occorre i parametri.

---
+++ parallel_version/parallel/rep_scores.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Nov  27 10:16:59 2019
+
+@author: Roberto Bellarosa
+"""
+
+#This class provide the reputation scores values
+
+class score:
+  def __init__(self):
+      return
+  
+  self.quest_up = 10 #question is voted up: +10
+  self.ans_up = 10 #answer is voted up: +10
+  self.ans_accepted = 15 #answer is marked "accepted": +15
+  self.acceptor = 2 #acceptor of the answer: +2
+  self.quest_down = -2 #question is voted down: −2
+  self.ans_down = -2 #your answer is voted down: −2
