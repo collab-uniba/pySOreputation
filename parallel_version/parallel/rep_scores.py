350306899b36f023c30339db3a826bf530992971
Roberto Bellarosa <37309982+RobertoBellarosa95@users.noreply.github.com>
2019-12-06 14:49:57 +0100

    Update rep_scores.py

--- parallel_version/parallel/rep_scores.py
+++ parallel_version/parallel/rep_scores.py
@@ -12,9 +12,9 @@ class score:
   def __init__(self):
       return
   
-  self.quest_up = 10 #question is voted up: +10
-  self.ans_up = 10 #answer is voted up: +10
-  self.ans_accepted = 15 #answer is marked "accepted": +15
-  self.acceptor = 2 #acceptor of the answer: +2
-  self.quest_down = -2 #question is voted down: −2
-  self.ans_down = -2 #your answer is voted down: −2
+  quest_up = 10 #question is voted up: +10
+  ans_up = 10 #answer is voted up: +10
+  ans_accepted = 15 #answer is marked "accepted": +15
+  acceptor = 2 #acceptor of the answer: +2
+  quest_down = -2 #question is voted down: −2
+  ans_down = -2 #your answer is voted down: −2
