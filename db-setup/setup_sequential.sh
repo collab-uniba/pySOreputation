b8c8eea23c535008197eb942d90f2aa2f2e49e36
Fabio Calefato <fcalefato@gmail.com>
2019-12-12 08:42:00 +0000

    Fix bad English

--- db-setup/setup_sequential.sh
+++ db-setup/setup_sequential.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-MYSQL_USER=insert_user_name
-MYSQL_PASS=insert_password
-MYSQL_SO_DB=insert_database_name
+MYSQL_USER=user_name
+MYSQL_PASS=password
+MYSQL_SO_DB=database_name
 
 echo "Creating tables and indexes (be patient, it does take a while...)"
 mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" --database="$MYSQL_SO_DB" < setup.sql
