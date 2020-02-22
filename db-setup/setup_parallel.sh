7231278b60fbfb9855a4450127f12d54d494efb6
Fabio Calefato <fcalefato@gmail.com>
2020-02-22 16:36:09 +0100

    Symlinking csv files for web service

--- db-setup/setup_parallel.sh
+++ db-setup/setup_parallel.sh
@@ -9,5 +9,11 @@ mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" --database="$MYSQL_SO_DB" < csv.
 
 echo "Moving CSV files to proper destinations (for both CLI client and web service server)"
 mv $MYSQL_SO_FOLDER/*.csv ../parallel 
+ln -s ../parallel_version/Users.csv ../SOWebService/Users.csv
+ln -s ../parallel_version/Question_Answer.csv ../SOWebService/Question_Answer.csv
+ln -s ../parallel_version/Posts_Votes1.csv ../SOWebService/Posts_Votes1.csv
+ln -s ../parallel_version/Posts_Votes2.csv ../SOWebService/Posts_Votes2.csv
+ln -s ../parallel_version/Posts_Votes3.csv ../SOWebService/Posts_Votes3.csv
+ln -s ../parallel_version/Posts_Votes4.csv ../SOWebService/Posts_Votes4.csv
 
 echo "Done."
