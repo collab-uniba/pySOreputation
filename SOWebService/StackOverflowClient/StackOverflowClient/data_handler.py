7b6fa44981ea070098de44706848874ba90ae755
Fabio Calefato <fcalefato@gmail.com>
2020-02-22 13:45:23 +0100

    Attempt to fix webservice client and server

--- SOWebService/StackOverflowClient/StackOverflowClient/data_handler.py
+++ SOWebService/StackOverflowClient/StackOverflowClient/data_handler.py
@@ -7,51 +7,50 @@ Created on Sat Nov 16 23:38:49 2019
 
 import json
 
+
 class data_handler:
-    
     file = "data_file.json"
-    
+
     def __init__(self):
         return
-        
+
     def to_json(self):
         self.my_obj = {}
         self.my_obj['user_id'] = []
         self.my_obj['date'] = []
         with open(self.file, "w") as write_file:
-            json.dump(self.my_obj,write_file)
-        
+            json.dump(self.my_obj, write_file)
+
     def update_json(self, user_id, date):
         self.my_obj['user_id'].append(user_id)
         self.my_obj['date'].append(date)
         with open(self.file, "w") as write_file:
-            json.dump(self.my_obj,write_file)
-            
+            json.dump(self.my_obj, write_file)
+
     def reset(self):
         self.my_obj['user_id'] = []
         self.my_obj['date'] = []
         with open(self.file, "w") as write_file:
-            json.dump(self.my_obj,write_file)
-    
+            json.dump(self.my_obj, write_file)
+
     def is_null(self):
-        if(len(self.my_obj['user_id']) == 0) and (len(self.my_obj['date']) == 0):
+        if (len(self.my_obj['user_id']) == 0) and (len(self.my_obj['date']) == 0):
             is_empty = True
         else:
             is_empty = False
         return is_empty
-    
+
     def give_json(self):
         return self.my_obj
-    
+
     def get_users(self):
         return self.my_obj['user_id']
-    
+
     def get_dates(self):
         return self.my_obj['dates']
-    
+
     def print_val(self):
         print(str(self.my_obj['user_id']))
-        
 
 # =============================================================================
 # result_json = data_handler()
--- SOWebService/StackOverflowServer/server1.py
+++ SOWebService/StackOverflowServer/server1.py
@@ -5,32 +5,53 @@ Created on Fri Nov 15 11:07:34 2019
 @author: Roberto Bellarosa
 """
 
-from parallel.procData import preprocessing
-from parallel.workers import worker
-from parallel.procData import get_basic_from_file
-from parallel.workers import decode_users
 import datetime
+
 from flask import Flask, request
 from flask import g
 
+from parallel.SOuser import SOuser
+from parallel.procData import get_basic_from_file
+from parallel.procData import preprocessing
+from parallel.workers import worker
+
 app = Flask(__name__)
+
 
 @app.route('/estimate', methods=['POST'])
 def serv_reputation():
     the_json = request.get_json(force=True)
     sousers = decode_users(the_json)
-    data = worker(the_map,basics,sousers)
+    data = worker(the_map, basics, sousers)
     return data
+
+
+def decode_users(json_data):
+    sousers = []
+    data = json_data
+    users = data['user_id']
+    dates = data['date']
+    i = 0
+    for item in users:
+        single = SOuser()
+        single.set_id(item)
+        single.set_end(dates[i])
+        sousers.append(single)
+        i = i + 1
+    return sousers
+
 
 def get_basic():
     if 'basics' not in g:
         g.basics = get_basic_from_file()
     return g.basics
 
+
 def get_map():
     if 'the_map' not in g:
         g.the_map = preprocessing()
     return g.the_map
+
 
 with app.app_context():
     print("\nWelcome to Stack Overflow reputation estimator!")
@@ -39,8 +60,7 @@ with app.app_context():
     the_map = get_map()
     basics = get_basic()
     end = datetime.datetime.now()
-    print("Time to load: " + str(end-start))
+    print("Time to load: " + str(end - start))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
-
