8a3e24d88029dc3e64b83df5717932c4c22a1b69
Fabio Calefato <fcalefato@gmail.com>
2020-02-22 10:52:14 +0100

    Fix: parallel version and webservice must use same CSV files

--- db-setup/setup_parallel.sh
+++ db-setup/setup_parallel.sh
@@ -8,7 +8,6 @@ echo "Creating CSV files into folder $MYSQL_SO_FOLDER (be patient, this takes a 
 mysql -u "$MYSQL_USER" --password="$MYSQL_PASS" --database="$MYSQL_SO_DB" < csv.sql
 
 echo "Moving CSV files to proper destinations (for both CLI client and web service server)"
-cp $MYSQL_SO_FOLDER/*.csv ../SOWebService/StackOverflowServer
 mv $MYSQL_SO_FOLDER/*.csv ../parallel 
 
 echo "Done."
