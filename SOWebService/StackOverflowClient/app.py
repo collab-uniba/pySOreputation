1f171f751f61375e7271061f3e939fbe5406523b
Fabio Calefato <fcalefato@gmail.com>
2020-02-23 21:01:56 +0100

    Improved error handling in client form

--- SOWebService/StackOverflowClient/app.py
+++ SOWebService/StackOverflowClient/app.py
@@ -18,21 +18,24 @@ def results():
     # response = None
     url = "http://{0}:{1}/estimate".format(app.config['WS_HOST'], app.config['WS_PORT'])
     if request.method == 'POST':
+        result = dict()
         param = request.form.to_dict()
-        if param['uids'] is "" or param['date'] is "":
-            return render_template('index.html', result="Error: All fields are mandatory")
-        elif not param['uids'].isdigit():
-            return render_template('index.html', result="Error: Id must be a number")
+        if not param['uids'].isdigit():
+            result['uids'] = param['uids']
+            result['date'] = param['date']
+            result['error'] = "Error: Id must be a number"
+            return render_template('index.html', result=result)
         elif not is_valid(param['date']):
-            return render_template('index.html',
-                                   result="Error: Invalid date format. Also, make sure it's >={0} and <={1}".format(
-                                       app.config['START_DATE'], app.config['DUMP_DATE']))
+            result['uids'] = param['uids']
+            result['date'] = param['date']
+            result['error'] = "Error: Invalid date format. Also, make sure it's >={0} and <={1}".format(
+                app.config['START_DATE'], app.config['DUMP_DATE'])
+            return render_template('index.html', result=result)
         else:
             json_input = json.dumps({"user_id": param['uids'], "date": param['date']})
             response = requests.post(url, data=json_input,
-                                          headers={'Content-Type': 'application/json'})
+                                     headers={'Content-Type': 'application/json'})
             if response.status_code == 200:
-                result = dict()
                 result[param['uids']] = json.loads(response.text)
                 return render_template('results.html', result=result)
 
@@ -44,6 +47,7 @@ def results():
                 #                      } """
             else:
                 return render_template('index.html', result="Error: {0}".format(response.status_code))
+
 
 def is_valid(date_text):
     valid = True
